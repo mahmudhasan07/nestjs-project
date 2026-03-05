b5a60bdac7533a05cb17ba14a2a76ce985110fdc
unknown <mahmudhasan.hb@gmail.com>
2026-03-05 06:00:31 +0600

    working on service system

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,15 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { AuthController } from './auth/auth.controller';
+import { CatsService } from './cats/cats.service';
+import { CatsModule } from './cats/cats.module';
+import { \[cats\]Module } from './[cats/]/[cats/].module';
+import { MahmudService } from './mahmud/mahmud.service';
+import { MahmudModule } from './mahmud/mahmud.module';
 
 @Module({
-  imports: [AuthModule],
+  imports: [AuthModule, CatsModule, \[cats\]Module, MahmudModule],
   controllers: [AppController, AuthController],
-  providers: [AppService],
+  providers: [AppService, CatsService, MahmudService],
 })
 export class AppModule {}
