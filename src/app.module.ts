6ec4838ea15da22d8448e832c33c5d1684bbae97
unknown <mahmudhasan.hb@gmail.com>
2026-06-19 20:52:59 +0600

    added

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { BookingModule } from './app/booking/booking.module';
 import { UserModule } from './app/user/user.module';
+import { PrismaModule } from './common/prisma/prisma.module';
 
 @Module({
-  imports: [BookingModule, UserModule],
+  imports: [BookingModule, UserModule, PrismaModule],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/common/prisma/prisma.service.ts
+++ src/common/prisma/prisma.service.ts
@@ -1,4 +1,9 @@
-import { Injectable, OnModuleDestroy, OnModuleInit } from '@nestjs/common';
+import {
+  Injectable,
+  Logger,
+  OnModuleDestroy,
+  OnModuleInit,
+} from '@nestjs/common';
 import { PrismaClient } from '@prisma/client';
 
 @Injectable()
@@ -6,8 +11,16 @@ export class PrismaService
   extends PrismaClient
   implements OnModuleInit, OnModuleDestroy
 {
+  private readonly logger = new Logger(PrismaService.name);
+
   async onModuleInit() {
-    await this.$connect();
+    try {
+      await this.$connect();
+      console.log('Database connected');
+    } catch (error) {
+      console.log(`Database failed to connect, ${error}`);
+      throw error;
+    }
   }
 
   async onModuleDestroy() {
